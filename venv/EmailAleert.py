f2b362621287d300bc4aa3ed15c122e58d9c5734
maorka <maor0525@gmail.com>
2020-10-16 14:17:19 +0300

    update Email allert module

---
+++ venv/EmailAleert.py
@@ -0,0 +1,20 @@
+import os
+import smtplib
+def MailAllertFunc(mail_allert):
+ gmail_user = "maor0525@gmail.com"
+ gmail_pwd = "May@5857335"
+ TO = 'maor0525@gmail.com'
+ SUBJECT = "PumbaParkingAllert"
+ TEXT = "Parking spot number 1  available at Shalom-Haliechem 32,Keren Hchayedet"
+ server = smtplib.SMTP('smtp.gmail.com', 587)
+ server.ehlo()
+ server.starttls()
+ server.login(gmail_user, gmail_pwd)
+ BODY = '\r\n'.join(['To: %s' % TO,
+        'From: %s' % gmail_user,
+        'Subject: %s' % SUBJECT,
+        '', TEXT])
+ server.sendmail(gmail_user, [TO], BODY)
+ print ('email sent')
+
+ return ()
